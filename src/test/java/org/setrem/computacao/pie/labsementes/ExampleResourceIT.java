22cf2b51b86980ccdf6c295d17fcfed4c4be780f
thalesgmartins <thalesdiniz30@gmail.com>
2025-10-23 15:43:12 -0300

    del arquivos exemplo

--- src/test/java/org/setrem/computacao/pie/labsementes/ExampleResourceIT.java
+++
@@ -1,8 +0,0 @@
-package org.setrem.computacao.pie.labsementes;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class ExampleResourceIT extends ExampleResourceTest {
-    // Execute the same tests but in packaged mode.
-}
