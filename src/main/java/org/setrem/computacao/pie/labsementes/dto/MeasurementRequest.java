97f12b896ce8ed8d05301b96db05e1d0869a73f5
thalesgmartins <thalesdiniz30@gmail.com>
2025-10-23 15:02:41 -0300

    add classe intermediária para validar Mesurements

---
+++ src/main/java/org/setrem/computacao/pie/labsementes/dto/MeasurementRequest.java
@@ -0,0 +1,14 @@
+package org.setrem.computacao.pie.labsementes.dto;
+
+import java.time.Instant;
+
+public class MeasurementRequest {
+
+    public Long sensorId;
+
+    public Double temperature;
+
+    public Double humidity;
+
+    public Instant time;
+}
