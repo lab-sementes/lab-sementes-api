a74c41b5003d439b18897ce933c27498248e0d9c
thalesgmartins <thalesdiniz30@gmail.com>
2025-11-03 23:47:44 -0300

    cria um arquivo especialmente para os sensores

---
+++ src/main/resources/db/migration/V1.0.0__create_sensor_table.sql
@@ -0,0 +1,20 @@
+
+-- Cria um tipo que simboliza o status do sensor (para não apagarmos do banco).
+CREATE TYPE sensor_status AS ENUM (
+    'Ativado',
+    'Desativado',
+    'Manutenção'
+);
+
+-- Cria um sensor, sem se preocupar com a Normalização do tipo e address
+CREATE TABLE IF NOT EXISTS sensor (
+    sensor_id SERIAL PRIMARY KEY,
+
+    sensor_name VARCHAR(255) NOT NULL UNIQUE,
+    sensor_type VARCHAR(100) NOT NULL,
+    unique_address VARCHAR(100) UNIQUE NULL,
+
+    status sensor_status NOT NULL DEFAULT 'Ativado',
+    data_criacao TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    data_ultima_mudanca_status TIMESTAMPTZ NULL
+);
