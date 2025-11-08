1b3813f452acadc55c368bb9a743b6b7910878d6
thalesgmartins <thalesdiniz30@gmail.com>
2025-11-08 01:46:00 -0300

    removido o Enum do banco. Será tratado apenas no backend

--- src/main/resources/db/migration/V1.0.0__create_sensor_table.sql
+++ src/main/resources/db/migration/V1.0.0__create_sensor_table.sql
@@ -1,11 +1,4 @@
 
--- Cria um tipo que simboliza o status do sensor (para não apagarmos do banco).
-CREATE TYPE sensor_status AS ENUM (
-    'Ativado',
-    'Desativado',
-    'Manutencao'
-);
-
 -- Cria um sensor, sem se preocupar com a Normalização do tipo e address
 CREATE TABLE IF NOT EXISTS sensor (
     sensor_id SERIAL PRIMARY KEY,
@@ -14,7 +7,7 @@ CREATE TABLE IF NOT EXISTS sensor (
     sensor_type VARCHAR(100) NOT NULL,
     unique_address VARCHAR(100) UNIQUE NULL,
 
-    status sensor_status NOT NULL DEFAULT 'Ativado',
+    status VARCHAR(50) NOT NULL DEFAULT 'Ativado',
     data_criacao TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     data_ultima_mudanca_status TIMESTAMPTZ NULL
 );
