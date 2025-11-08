307f542d9c04b5b754dea238a02605e5cf6cb980
thalesgmartins <thalesdiniz30@gmail.com>
2025-11-08 01:50:56 -0300

    novo controller, usando novas classes

--- src/main/java/br/com/setrem/computacao/pie/labsementes/controller/MeasurementController.java
+++ src/main/java/br/com/setrem/computacao/pie/labsementes/controller/MeasurementController.java
@@ -1,57 +1,72 @@
 package br.com.setrem.computacao.pie.labsementes.controller;
 
+import br.com.setrem.computacao.pie.labsementes.dto.SensorDataAggregate;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
 import org.jboss.resteasy.reactive.RestResponse;
-import br.com.setrem.computacao.pie.labsementes.dto.MeasurementRequest;
+import br.com.setrem.computacao.pie.labsementes.dto.MeasurementDTO;
 import br.com.setrem.computacao.pie.labsementes.model.Measurement;
 import br.com.setrem.computacao.pie.labsementes.service.MeasurementService;
 
+import java.awt.*;
 import java.time.Instant;
 import java.util.List;
 
 @Path("/measurements")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class MeasurementController {
+
     @Inject
     MeasurementService measurementService;
 
-
+    // --- Endpoints Panache
     @POST
-    public RestResponse<Measurement> create(MeasurementRequest dto) {
+    public RestResponse<Measurement> criar(MeasurementDTO dto) {
         try {
             Measurement newMeasurement = measurementService.create(dto);
-            return RestResponse.status(RestResponse.Status.CREATED, newMeasurement);
+            return RestResponse.ResponseBuilder.ok(newMeasurement).status(RestResponse.Status.CREATED).build();
         } catch (NotFoundException e) {
-            return RestResponse.status(RestResponse.Status.NOT_FOUND, null);
+            return RestResponse.notFound();
+        } catch (Exception e) {
+            return RestResponse.status(RestResponse.Status.BAD_REQUEST);
         }
     }
 
     @GET
-    @Path("/sensor/{sensorId}")
-    public RestResponse<List<Measurement>> getBySensor(
-            @PathParam("sensorId") Long sensorId,
-            @QueryParam("latest") boolean latest,
-            @QueryParam("start") Instant start,
-            @QueryParam("end") Instant end,
-            @QueryParam("page") @DefaultValue("0") int page,
-            @QueryParam("size") @DefaultValue("50") int size) {
+    @Path("/{sensorId}/{ts}")
+    public RestResponse<Measurement> buscarPorId(
+            @PathParam("sensorId") Integer sensorId,
+            @PathParam("ts") Instant ts) {
+        return measurementService.findById(sensorId, ts)
+                .map(measurement -> RestResponse.ResponseBuilder.ok(measurement).build())
+                .orElse(RestResponse.status(RestResponse.Status.NOT_FOUND));
+    }
 
-        if (latest) {
-            // Se ?latest=true, retorna só a última (ou 404)
-            return measurementService.getLatestForSensor(sensorId)
-                    .map(List::of) // Converte o Optional<Measurement> para List<Measurement>
-                    .map(RestResponse::ok)
-                    .orElse(RestResponse.status(RestResponse.Status.NOT_FOUND));
+    // --- Endpoints DataSource
+
+    @GET
+    @Path("/{sensorId}/aggregates")
+    public RestResponse<List<SensorDataAggregate>> buscarAgregacao(
+            @PathParam("sensorId") Integer sensorId,
+            @QueryParam("bucket") @DefaultValue("1 hour") String bucketDuration) {
+
+        List<SensorDataAggregate> agregados = measurementService.getAggregates(sensorId, bucketDuration);
+
+        if (agregados.isEmpty()) {
+            return RestResponse.noContent();
         }
 
-        if (start != null && end != null) {
-            // Se ?start=...&end=..., busca por intervalo
-            List<Measurement> measurements = measurementService.getForSensorInTimeRange(sensorId, start, end);
-            return RestResponse.ok(measurements);
-        }
+        return RestResponse.ResponseBuilder.ok(agregados).build();
+    }
 
-        // Se não, faz a busca paginada padrão
-        List<Measurement> measurements = measurementService.getForSensor(sensorId, page, size);
-        return RestResponse.ok(measurements);
+    @GET
+    @Path("/{sensorId}/latest")
+    public RestResponse<Measurement> getLatestMeasurement(@PathParam("sensorId") Integer sensorId) {
+
+        return measurementService.findLatestMeasurement(sensorId)
+                .map(measurement -> RestResponse.ResponseBuilder.ok(measurement).build())
+                .orElse(RestResponse.status(RestResponse.Status.NOT_FOUND));
     }
 }
